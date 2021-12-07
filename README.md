adab25401fe18715b6d87c85cde2292bfa2e3ab7
fernandeslouro <jd.louro@outlook.pt>
2021-12-07 22:01:54 +0000

    more concrete development taking arduino's pins into account

--- README.md
+++ README.md
@@ -3,26 +3,27 @@
 Building a custom controller board for our family's pomace boiler. 
 
 What the board will need to control:
- - motor to get pomace into the fire (according to light sensor and thermometer)
- - fan motor which blows air into the flame (according to light sensor and thermometer)
- - motors bring water from the house (controlled by termometer and requests in the house)
- - motor to bring pomace into pyramidal storage (according to a light/fullness sensor to be installed)
+ - motor to get pomace into the fire (relay, according to light sensor and thermometer) - pin 7
+ - fan motor which blows air into the flame (speed controlled, according to light sensor and thermometer)
+ - motors bring water from the house (relay, controlled by termometer and requests in the house) - pin 8
+ - motor to bring pomace into pyramidal storage (relay, according to a light/fullness sensor to be installed)
  - motor to crunch pomace and bring it to the larger storage (user-actioned, but must add a protection against stalling)
  - heat gun turn on/off (maybe)
+ - Will need a clock as well
 
 Sensors the board will have to process:
- - Water temperature
- - Waste gas temperature
- - Temperature of the waters going through the house (sanitary hot water)
- - Temperature insde the house
- - Flame light
+ - Water temperature (AO)
+ - Temperature of the waters going through the house (sanitary hot water) (A1)
+ - Temperature insde the house (A2)
+ - Waste gas temperature (A3)
+ - Flame light (A4)
  - Fullness of pyramid storage 
  - Stalling of the motors (especially the pomace breaker)
 
 Material needed:
  - Arduino board
  - Screen
- - A bunch of 
+ - A bunch of relays
 
-
-Boiler algorithm
+Web server should be able to:
+ - Show the present state of the boiler
--- algorithm.ino
+++ algorithm.ino
@@ -1,6 +1,45 @@
+// add necessary constants
+//#define PI 3.14159
+ 
+double temp_measure(int pin){
+
+    const double beta = 3600.0;
+    const double r0 = 10000.0;
+    const double t0 = 273.0 + 25.0;
+    const double rx = r0 * exp(-beta/t0);
+ 
+    // Circuit parameters
+    const double vcc = 5.0;
+    const double R = 20000.0;
+ 
+    // Number of read ramples
+    const int nSamples = 5;
+ 
+    int sum = 0;
+    for (int i = 0; i < nSamples; i++) {
+        sum += analogRead(pin);
+        delay (10);
+    }
+ 
+    double v = (vcc*sum)/(nSamples*1024.0);
+    double rt = (vcc*R)/v - R;
+ 
+    return beta / log(rt/rx);
+}
+
 
 
 void setup(){
+
+    const int pinBoilerTemp = A0;
+    const int pinHotWaterTemp = A1;
+    const int pinHouseTemp = A2;
+    const int pinWasteGasTemp = A3;
+    const int pinFlameLight = A4;
+
+    // Configures specified pin to work as input ou output
+    pinMode(7, OUTPUT);// connected to S terminal of Relay
+    pinMode(8, OUTPUT);
 
     int fan_speed;
     bool fire_motor;
@@ -17,7 +56,7 @@ void setup(){
     // check if light is on
     while(average_light < 50){
         for (int counts=0; counts<10; counts++){
-            average_light+= light_sensor_value;
+            average_light+= analogRead(pinFlameLight);
             delay(1000);
         }
         average_light/=10;
@@ -32,10 +71,11 @@ class gear_values{
         int motor_stopped;
 };
 
-void gear(bool fire_motor, gear_values *present_gear){
-    fire_motor = true;
+void gear(gear_values *present_gear){
+
+    digitalWrite(7,HIGH);// turn relay ON
     delay(present_gear->motor_working * 1000);
-    fire_motor = false;
+    digitalWrite(7, LOW);// turn relay OFF
     delay(present_gear->motor_stopped * 1000);
 }
 
@@ -44,19 +84,47 @@ void loop(){
     //check temperature of boiler every 10 seconds, and chage mode appropriately
     int boiler_temp;
     int boiler_speed;
+    int house_temp;
+    bool ac_pump;
 
     gear_values *low;
     gear_values *medium;
     gear_values *high;
 
-    while(true){
-        if (boiler_temp < 55){
-            gear(fire_motor, high);
-        } else if (boiler_temp > 55 && boiler_temp < 65){
-            gear(fire_motor, medium);
-        } else if (boiler_temp >65){
-            gear(fire_motor, low);
+        while(true){
+            boiler_temp = temp_measure(pinBoilerTemp);
+
+            if (boiler_temp < 55){
+                gear(high);
+            } else if (boiler_temp > 55 && boiler_temp < 65){
+                gear(medium);
+            } else if (boiler_temp >65){
+                gear(low);
+            }
+
+            
+            if (temp_measure(pinHouseTemp) < 18){
+                digitalWrite(8,HIGH);
+            } else{
+                digitalWrite(8,LOW);
+            }
+
+            if (temp_measure(pinHotWaterTemp) < 60){
+                digitalWrite(8,HIGH);
+            } else{
+                digitalWrite(8,LOW);
+            }
+
+            do {
+                for (int counts=0; counts<10; counts++){
+                    average_light+= light_sensor_value;
+                    delay(1000);
+                }
+                average_light/=10;
+            } while(average_light > 50)
+
         }
+
     }
 
 }
